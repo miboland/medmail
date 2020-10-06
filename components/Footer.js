3620b5a9d2361e357886dc05a6f19dac0e03ec6f
Michael Boland <michael.boland@mogul.gg>
2020-10-06 12:10:10 -0500

    Add download link.

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,19 @@ import { Flex, Link, IconButton } from "@chakra-ui/core";
 
 const Footer = () => (
   <Flex justify="center" mb={4}>
+    <Link
+      href="https://drive.google.com/uc?export=download&id=1fBCOMzLA43XabJkZaTdCqT_7pakbJdaA"
+      title="Electron"
+      isExternal
+    >
+      <IconButton
+        aria-label="Electron"
+        icon="download"
+        size="lg"
+        color="gray.500"
+        variant="ghost"
+      />
+    </Link>
     <Link href="https://github.com/miboland/medmail" title="GitHub" isExternal>
       <IconButton
         aria-label="GitHub"
