3b46ff29d714e5cc98087314a4cd970baf96bb78
Michael Boland <michael.boland@mogul.gg>
2020-11-01 19:55:12 -0600

    Fix download link.

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,12 @@ import { Flex, Link, IconButton } from "@chakra-ui/core";
 const Footer = () => (
   <Flex justify="center" mb={4}>
     <Link
-      href="https://drive.google.com/uc?export=download&id=1fQ0PCb4IxPwfPE818GOSJ3FvCoBGFV_Y"
-      title="Electron"
+      href="https://github.com/miboland/medmailectron"
+      title="Download"
       isExternal
     >
       <IconButton
-        aria-label="Electron"
+        aria-label="Download"
         icon="download"
         size="lg"
         color="gray.500"
